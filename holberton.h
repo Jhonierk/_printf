a4e76ec4b1863776f0d02e1b950d6f45be457356
vanessasot <vannexxaa@gmail.com>
2020-07-20 23:17:17 +0000

    Create struct printf

--- holberton.h
+++ holberton.h
@@ -1,12 +1,29 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+#define BUFFER_SIZE 1024
 
 /* Standard Libraries*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdarg.h>
+
+/**
+ * struct t_types - Struct format
+ *
+ * @type: Arguments
+ * @print: The function associated
+ *
+ * Description: Define a new type struct t_types
+ */
+
+typedef struct t_types
+{
+	char *type;
+	int (*print)();
+} f_types;
 
 /* Prototypes */
 
