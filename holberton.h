5368610bd0465466b56f44e7f30837aa5582e358
vanessasot <vannexxaa@gmail.com>
2020-07-20 21:13:06 +0000

    Create header

---
+++ holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* HOLBERTON */
