7a1c05f7f6385af8fdf7ecdd9050920713912886
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-07-21 10:53:51 -0500

    edit spaces

--- func_generic.c
+++ func_generic.c
@@ -11,12 +11,10 @@
 char *_conect_char(char *connetc, char *src)
 {
 	int post_connect = 0, pos_src = 0;
-	
 	while(connetc[post_connect] != '\0')
 	{
 		post_connect++;
 	}
-	
 	while(src[post_connect] != '\0')
     	{
 		connetc[post_connect + pos_src] = src[pos_src];
