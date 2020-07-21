0606c24b1d61f047300b3a19d233a7ec8ef7a5c3
vanessasot <vannexxaa@gmail.com>
2020-07-21 02:54:19 +0000

    Inicialization struc

---
+++ _formats.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _formats - Select the function according to a format.
+ * @form: Pointer to the format.
+ *
+ * Return: 0 (success).
+ */
+
+int (*_formats(char form))()
+{
+	f_types op[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	int runs_op;
+
+	for (runs_op = 0; op[runs_op].type; runs_op++)
+	{
+		if (op[runs_op].type[0] == form)
+			return (op[runs_op].print);
+	}
+	return (NULL);
+}
